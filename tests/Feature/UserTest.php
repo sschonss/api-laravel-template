0d8dff943bda1689005080921f105ad2d5988a72
sschonss <schonsluuiz@gmail.com>
2024-03-18 23:06:23 -0300

    Initial commit

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+class UserTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate'); // Executar migrações antes dos testes
+    }
+    private $token;
+    public function test_the_register_user_endpoint_returns_a_successful_response(): void
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john@john.com',
+            'password' => 'password',
+            ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'status' => 'success'
+        ]);
+
+    }
+
+    public function test_the_login_user_endpoint_returns_a_successful_response(): void
+    {
+        $this->post('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john@john.com',
+            'password' => 'password',
+        ]);
+
+        $response = $this->post('/api/login', [
+            'email' => 'john@john.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+
+        $this->token = $response->json('access_token');
+
+    }
+}
